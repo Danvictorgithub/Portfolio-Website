03c18c489c081f652105ede63a9355927e5cbb85
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-03-23 21:25:38 +0800

    Added Experience Timeline

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -2,7 +2,7 @@
 	<section class="hero">
 		<img src="./myProfile.jpg" alt="myPicture">
 		<div class="hero-desc">
-			<h1>Hi! My name is <span class="sp">Dan</span></h1>
+			<h2>Hi! My name is <span class="sp">Dan</span></h2>
 			<em>Web Developer</em>
 		</div>
 		<p class="description gradient-b">Developing Full-Stack Application, Designing Products with <u><span class="sp sp-b gradient-o">Minimalism Approach</span></u></p>
@@ -12,7 +12,21 @@
 			<p>LinkedIn</p> <img src="./link-box.svg" alt="linkIco">
 		</a>
 	</section>
-	<section></section>
+	<section class="experience">
+		<header class="experience-h"><h3>Working Experience</h3></header>
+		<div class="timeline-container">
+			<div class="alt-vtl">
+			  <div class="alt-event">
+			    <p class="date">Github Contributor - 2022</p>
+			    <p class="txt">Worked with Students, creating a Budget Tracker Application. First exposure to Github Workflow</p>
+			  </div>
+			  <div class="alt-event right">
+			    <p class="date">Prifina Front-End Developer - 2022/2023</p>
+			    <p class="txt">Worked as React Developer creating Widget Application with their Data from oura Ring, fitbit, and other fitness apps. Used Jest and StoryBookJS for automated testing.</p>
+			  </div>
+			</div>
+		</div>
+	</section>
 </article>
 
 <style>
@@ -50,12 +64,12 @@
 	}
 	.gradient-b {
 		background: -webkit-linear-gradient(white,lightgray, #333);
-		-webkit-background-clip: text;
+		background-clip: text;
 		-webkit-text-fill-color: transparent;
 	}
 	.gradient-o {
 		background: -webkit-linear-gradient(orange, var(--orange-m));
-		-webkit-background-clip: text;
+		background-clip: text;
 		-webkit-text-fill-color: transparent;
 	}
 	.description {
@@ -84,6 +98,80 @@
 		margin-left:12px;
 		filter:invert(100%);
 	}
+	.experience {
+		position:relative;
+		width:100%;
+		color:white;
+	}
+	.experience-h {
+		background-color: #202020;
+		height: 60px;
+		display:flex;
+		justify-content:center;
+		align-items:center;
+		border-radius: 12px;
+	}
+/*	Timeline CSS*/
+	.timeline-container {
+		padding: 12px;
+	}
+	.alt-vtl {
+		position: relative;
+		max-width: 800px;
+		margin: 0 auto;
+	}
+	.alt-vtl::before {
+		content: "";
+		width: 5px;
+		background-color: lightgray;
+		position: absolute;
+		top: 0; bottom: 0; left: 50%;
+	}
+
+	div.alt-event {
+	  background-color: #1E1F22;
+	  border-radius: 6px;
+	  padding: 20px 30px;
+	  width: 45%;
+	  margin-bottom: 10px;
+	  position: relative;
+	  left: 0;
+	}
+	div.right { left: 55%; }
+	p.date {
+	  font-size: 1.1em;
+	  font-weight: 700;
+	  color: white;
+	}
+	p.txt {
+	  margin: 10px 0 0 0;
+	  color: white;
+	}
+	div.alt-event::before, div.alt-event.right::before {
+	  content: "";
+	  border: 10px solid transparent;
+	  position: absolute;
+	}
+	div.alt-event::before {
+	  border-left-color: #545454;
+	  border-right: 0;
+	  left: 100%; top: 20%;
+	}
+	div.alt-event.right::before {
+	  border-right-color: #545454;
+	  border-left: 0;
+	  left: auto; right: 100%; top: 20%;
+	}
+	div.alt-event::after, div.alt-event.right::after {
+	  content: "";
+	  background: black;
+	  border: 4px solid white;
+	  width: 16px; height: 16px;
+	  border-radius: 50%;
+	  position: absolute;
+	}
+	div.alt-event::after { top: 20%; left: 109.8%; }
+	div.alt-event.right::after { top: 20%; left: auto; right: 108.1%; }
 	@media (max-width:600px) {
 		.description {
 			font-size:1.8rem;
